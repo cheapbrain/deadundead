aaa23d90910255d2aa44f0c4fbbd57bc5c907052
Kroj <simo.gnani@gmail.com>
2016-11-18 12:33:08 +0100

    Sistema entità 2.0
    Rifatto sistema entità da zero

--- code/src/Entities.cpp
+++ code/src/Entities.cpp
@@ -1,33 +1,156 @@
-#include "Entities.h"
+#include <stdlib.h>
+#include <malloc.h>
+#include "entities.h"
 
-int createEntity(World *world) {
-	for (int entity = 0; entity < MAX_ENTITIES; entity++) {
-		if (world->mask[entity] == COMPONENT_NONE) { //la prima entity che trova senza componenti non è un entity, è un buco
-			return(entity);
-		}
+//
+//COMMON
+//
+Type get_type(const void *e) {
+	return *(Type*)e;
+}
+int is_in(Type t, Type_list list) {
+	for (int i = 0; i < list->size; i++)
+		if (list->list[i] == t) return 1;
+	return 0;
+}
+int colliding(const void* entity_1, const void* entity_2) {
+	Entity e1 = (Entity)entity_1;
+	Entity e2 = (Entity)entity_2;
+	if (collisions[e1->type*ENTITY_NUMBER + e2->type] == 1) { //devono collidere
+		//TO-DO collisione
+		return 0;
 	}
-	//printf("Error!  No more entities left!\n");
-	return(MAX_ENTITIES);
+	else return 0;
 }
 
-void destroyEntity(World *world, int entity) {
-	world->mask[entity] = COMPONENT_NONE;
+//
+//TYPE_LIST
+//
+Type_list new_type_list(int size) {
+	Type_list l = (Type_list)malloc(sizeof(Type_list_struct));
+	l->size = size;
+	l->list = (Type*)malloc(sizeof(Type)*size);
+	return l;
 }
 
-int createPlatform(World *world, float x, float y, float mass, float elasticity) {
+//
+//ENTITY
+//
+Entity new_entity() {
+	Entity e = (Entity)malloc(sizeof(Entity_struct));
+	e->id = 0;
+	return e;
 }
 
+//
+//PLATFORM
+//
+Platform new_platform() {
+	Platform p = (Platform)malloc(sizeof(Platform_struct));
+	p->e = new_entity();
+	p->e->type = PLATFORM;
+	p->update = &update_platform;
+	return p;
+}
+void update_platform(const void* self) {
+	Platform ego = (Platform)self;
+}
 
-int createPlayer(World *world, float x, float y, char* name){
-	int entity = createEntity(world);
-	world->mask[entity] = COMPONENT_POSITION | COMPONENT_VELOCITY | COMPONENT_APPEARANCE | COMPONENT_BODY | COMPONENT_COLLISION | COMPONENT_PLAYER;
-	world->positionComponents[entity].x = x;
-	world->positionComponents[entity].y = y;
-	world->velocityComponents[entity].speedX = 0;
-	world->velocityComponents[entity].speedY = 0;
-	//manca appearance e collision
-	world->bodyComponents[entity].mass = 10;
-	world->bodyComponents[entity].elasticity = .7f;
-	world->playerComponents[entity].name = name;
-	return(entity);
+//
+//PLAYER
+//
+Player new_player() {
+	Player p = (Player)malloc(sizeof(Player_struct));
+	p->e = new_entity();
+	p->e->type = PLAYER;
+	p->update = &update_player;
+	p->collides = &colliding;
+	return p;
+}
+void update_player(const void* self) {
+	Player ego = (Player)self;
+}
+
+//
+//WEAPON
+//
+Weapon new_weapon() {
+	Weapon w = (Weapon)malloc(sizeof(Weapon_struct));
+	w->e = new_entity();
+	w->e->type = WEAPON;
+	w->update = &update_weapon;
+	return w;
+}
+void update_weapon(const void* self) {
+	Weapon ego = (Weapon)self;
+}
+
+//
+//ACTIVE
+//
+Active new_active() {
+	Active w = (Active)malloc(sizeof(Active_struct));
+	w->e = new_entity();
+	w->e->type = ACTIVE;
+	w->update = &update_active;
+	return w;
+}
+void update_active(const void* self) {
+	Active ego = (Active)self;
+}
+
+//
+//PASSIVE
+//
+Passive new_passive() {
+	Passive w = (Passive)malloc(sizeof(Passive_struct));
+	w->e = new_entity();
+	w->e->type = PASSIVE;
+	w->update = &update_passive;
+	return w;
+}
+void update_passive(const void* self) {
+	Passive ego = (Passive)self;
+}
+
+//
+//CONSUMABLE
+//
+Consumable new_consumable() {
+	Consumable w = (Consumable)malloc(sizeof(Consumable_struct));
+	w->e = new_entity();
+	w->e->type = CONSUMABLE;
+	w->update = &update_consumable;
+	return w;
+}
+void update_consumable(const void* self) {
+	Consumable ego = (Consumable)self;
+}
+
+//
+//CONTROLLER
+//
+Controller new_controller() {
+	Controller w = (Controller)malloc(sizeof(Controller_struct));
+	w->e = new_entity();
+	w->e->type = CONTROLLER;
+	w->update = &update_controller;
+	return w;
+}
+void update_controller(const void* self) {
+	Controller ego = (Controller)self;
+}
+
+//
+//PROJECTILE
+//
+Projectile new_projectile() {
+	Projectile w = (Projectile)malloc(sizeof(Projectile_struct));
+	w->e = new_entity();
+	w->e->type = PROJECTILE;
+	w->update = &update_projectile;
+	return w;
+}
+void update_projectile(const void* self) {
+	Projectile ego = (Projectile)self;
 }
--- code/src/Entities.h
+++ code/src/Entities.h
@@ -1,23 +1,113 @@
-#include "Components.h"
+#pragma once
 
-#define MAX_ENTITIES 1000
+#define ENTITY_NUMBER 8
 
-struct World {
-	int mask[MAX_ENTITIES];
-	PositionComponent positionComponents[MAX_ENTITIES];
-	VelocityComponent velocityComponents[MAX_ENTITIES];
-	AppearanceComponent appearanceComponents[MAX_ENTITIES];
-	BodyComponent bodyComponents[MAX_ENTITIES];
-	CollisionComponent collisionComponents[MAX_ENTITIES];
-	PlayerComponent playerComponents[MAX_ENTITIES];
+const int collisions[ENTITY_NUMBER*ENTITY_NUMBER] = {
+	0,1,0,0,0,0,0,1,
+	1,0,0,1,1,0,0,1,
+	0,0,0,0,0,0,0,0,
+	0,1,0,0,0,0,0,1,
+	0,1,0,0,0,0,0,1,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	1,1,0,1,1,0,0,0
 };
 
-//creazione e distruzione nuova entità generica
-int createEntity(World *world);
-void destroyEntity(World *world, int entity);
+enum Type {
+	PLATFORM,
+	PLAYER,
+	WEAPON,
+	ACTIVE,
+	PASSIVE,
+	CONSUMABLE,
+	CONTROLLER,
+	PROJECTILE
+};
 
-int createPlayer(World *world, float x, float y, char* name); //mancano parametri
+struct Type_list_struct {
+	Type* list;
+	int size;
+};
+typedef Type_list_struct* Type_list;
+Type_list new_type_list(int);
 
+//
+//TIPI FUNZIONI: 1a LETTERA TIPO RITORNO, POI TIPI INGRESSO
+// v void, i int, c char, f float, d double (puntatori omessi)
+//
+typedef void(*vv)(const void*);
+typedef int(*ivv)(const void*, const void*);
 
+struct Entity_struct {
+	Type type;
+	int id;
+	float x, y;
+};
+typedef Entity_struct* Entity;
+Entity new_entity();
 
+struct Platform_struct {
+	Entity e;
+	vv update;
+};
+typedef Platform_struct* Platform;
+Platform new_platform();
+void update_platform(const void*);
 
+struct Player_struct {
+	Entity e;
+	char *name;
+	vv update;
+	ivv collides;
+};
+typedef Player_struct* Player;
+Player new_player();
+void update_player(const void*);
+
+struct Weapon_struct {
+	Entity e;
+	vv update;
+};
+typedef Weapon_struct* Weapon;
+Weapon new_weapon();
+void update_weapon(const void*);
+
+struct Active_struct {
+	Entity e;
+	vv update;
+};
+typedef Active_struct* Active;
+Active new_active();
+void update_active(const void*);
+
+struct Passive_struct {
+	Entity e;
+	vv update;
+};
+typedef Passive_struct* Passive;
+Passive new_passive();
+void update_passive(const void*);
+
+struct Consumable_struct {
+	Entity e;
+	vv update;
+};
+typedef Consumable_struct* Consumable;
+Consumable new_consumable();
+void update_consumable(const void*);
+
+struct Controller_struct {
+	Entity e;
+	vv update;
+};
+typedef Controller_struct* Controller;
+Controller new_controller();
+void update_controller(const void*);
+
+struct Projectile_struct {
+	Entity e;
+	vv update;
+};
+typedef Projectile_struct* Projectile;
+Projectile new_projectile();
+void update_projectile(const void*);
