f7175a4f37286a9fe11326217426f7fe1e343e92
Kroj <simo.gnani@gmail.com>
2016-11-23 21:53:11 +0100

    Sistema entità 2.1.1

--- code/src/Entities.cpp
+++ code/src/Entities.cpp
@@ -5,14 +5,8 @@
 //
 //COMMON
 //
-Type get_type(const void *super) {
-	return *(Type*)super;
-}
 vvv get_interaction(Interactable i) {
-	switch (i->type) {
-	case PICKUP: return pickup; break;
-	case STEADY: return steady_interactions[i->id];	break;
-	}
+	return steady_interactions[i->type];
 }
 void move(const void* entity) {
 	Projectile e = (Projectile)entity;
@@ -20,11 +14,7 @@ void move(const void* entity) {
 	e->y += e->speed_y;
 }
 int colliding(const void* entity_1, const void* entity_2) {
-	Type t1=get_type(entity_1), t2=get_type(entity_2);
-	if (collisions[t1*TYPES_NUMBER + t2] == 1)
-		return 0; //TO-DO collisione
-	else 
-		return 0;
+	return 0;
 }
 
 //
@@ -32,7 +22,6 @@ int colliding(const void* entity_1, const void* entity_2) {
 //
 Platform new_platform() {
 	Platform p = (Platform)malloc(sizeof(Platform_struct));
-	p->type = PLATFORM;
 	p->x = 0;
 	p->y = 0;
 	p->update = &update_platform;
@@ -47,7 +36,6 @@ void update_platform(const void* self, float delta) {
 //
 Projectile new_projectile() {
 	Projectile p = (Projectile)malloc(sizeof(Projectile_struct));
-	p->type = PROJECTILE;
 	p->update = &update_projectile;
 	p->colliding = &colliding;
 	p->move = &move;
@@ -60,13 +48,12 @@ void update_projectile(const void* self, float delta) {
 //
 //INTERACTABLE
 //
-Interactable new_interactable(Type t, int id) {
-	if (id>=0) {
+Interactable new_interactable(Interactable_type type, Platform p) {
+	if (type>=0) {
 		Interactable i = (Interactable)malloc(sizeof(Interactable_struct));
-		i->type = t;
-		i->id = 0;
-		i->x = 0;
-		i->y = 0;
+		i->x = p->x;
+		i->y = p->y;
+		i->type = type;
 		//TO-DO
 		//i->update;
 		i->interact = get_interaction(i);
@@ -76,16 +63,32 @@ Interactable new_interactable(Type t, int id) {
 }
 
 //
+//PICKUP
+//
+Pickup new_pickup(Pickup_type type) {
+	if (type) {
+		Pickup p = (Pickup)malloc(sizeof(Pickup_struct));
+		p->x = 0;
+		p->y = 0;
+		p->type = type;
+		//TO-DO
+		//i->update;
+		p->pickupped = &pickupped;
+		return p;
+	}
+	else return NULL;
+}
+
+//
 //PLAYER
 //
 Player new_player(char* name) {
 	Player p = (Player)malloc(sizeof(Player_struct));
-	p->type = PLAYER;
 	p->x = 0;
 	p->y = 0;
 	p->speed_x = 0;
 	p->speed_y = 0;
-	p->held = -1;
+	p->held = NOTHING;
 	p->health = 100.f;
 	p->name = name;
 	p->update = &update_player;
@@ -96,20 +99,15 @@ Player new_player(char* name) {
 void update_player(const void* self, float delta) {
 	Player ego = (Player)self;
 }
-void interact_player(const void* self, const void* target) {
-	Player player = (Player)self;
-	Interactable interactable = (Interactable)target;
-	interactable->interact(interactable, player);
-}
 
 //INTERAZIONI
-void pickup(const void* entity_1, const void* entity_2) {
-	Interactable item = (Interactable)entity_1;
+void pickupped(const void* entity_1, const void* entity_2) {
+	Pickup item = (Pickup)entity_1;
 	Player player = (Player)entity_2;
-	int held = player->held;
-	player->held = item->id;
+	Pickup_type held = player->held;
+	player->held = item->type;
 	free(item);
-	item = new_interactable(PICKUP, held);
+	item = new_pickup(held);
 }
 void boost(const void* entity_1, const void* entity_2) {
 	Interactable item = (Interactable)entity_1;
@@ -123,30 +121,27 @@ void damage(const void* entity_1, const void* entity_2) {
 }
 
 
-
-#define PLAYERS_NUM 4
-#define PLATFORMS_NUM 10
-#define INTERACTABLES_NUM 3
 void prova() {
 	//inizializzazioni varie
 	int i;
 	float delta = 0; //ottenuto in altra maniera
-	Platform platforms[PLATFORMS_NUM];
-	Player players[PLAYERS_NUM];
-	Interactable interactables[INTERACTABLES_NUM];
-	for (i = 0; i < PLATFORMS_NUM; i++)
+	Platform platforms[10+2];
+	Player players[4];
+	Pickup pickups[1];
+	Interactable interactables[2];
+	for (i = 2; i < 10+2; i++)
 		platforms[i]=new_platform();
-	for (i = 0; i < PLAYERS_NUM; i++)
+	for (i = 0; i < 4; i++)
 		players[i] = new_player((char*)(&i));
-	interactables[0] = new_interactable(PICKUP, 0);
-	interactables[1] = new_interactable(STEADY, 0);
-	interactables[2] = new_interactable(STEADY, 1);
+
+	pickups[0] = new_pickup(WEAPON);
+	interactables[0] = new_interactable(TRAP, platforms[0]);
+	interactables[1] = new_interactable(BOUNCING, platforms[1]);
 	
 	//questa dovrebbe raccogliere l'item
-	players[0]->interact(players[0], interactables[0]);
+	pickups[0]->pickupped(pickups[0], players[0]);
 	//questa dovrebbe fare danno al player
-	players[1]->interact(players[1], interactables[1]);
+	interactables[0]->interact(interactables[0], players[0]);
 	//questa dovrebbe lanciare il player in alto
-	players[0]->interact(players[2], interactables[2]);
-
+	interactables[1]->interact(interactables[1], players[1]);
 }
